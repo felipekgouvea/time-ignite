44b4faf83790b17365945000db96e93bfb550607
felipekgouvea <felipekinupesg@hotmail.com>
2023-07-27 14:19:43 -0300

    refactor: Home Page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,59 @@
+import { Play } from 'phosphor-react'
+import {
+  FormContainer,
+  HomeContainer,
+  CountDownContainer,
+  Separetor,
+  StarCountDownButton,
+  MinutesAmountInput,
+  TaskInput,
+} from './styles'
+
 export const Home = () => {
   return (
-    <div>
-      <h1>Página Home</h1>
-    </div>
+    <HomeContainer>
+      <form action="">
+        <FormContainer>
+          <label htmlFor="task">Vou trabalhar em</label>
+          <TaskInput
+            id="taks"
+            list="task-suggestions"
+            placeholder="Dê um nome para o seu projeto"
+          />
+
+          <datalist id="task-suggestions">
+            <option value="Projeto 1" />
+            <option value="Projeto 2" />
+            <option value="Projeto 3" />
+            <option value="Projeto 4" />
+          </datalist>
+
+          <label htmlFor="minutesAmount">durante</label>
+          <MinutesAmountInput
+            type="number"
+            id="minutesAmount"
+            placeholder="00"
+            step={5}
+            min={5}
+            max={60}
+          />
+
+          <span>minutos.</span>
+        </FormContainer>
+
+        <CountDownContainer>
+          <span>0</span>
+          <span>0</span>
+          <Separetor>:</Separetor>
+          <span>0</span>
+          <span>0</span>
+        </CountDownContainer>
+
+        <StarCountDownButton type="submit">
+          <Play size={24} />
+          Comerçar
+        </StarCountDownButton>
+      </form>
+    </HomeContainer>
   )
 }
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,1 +1,117 @@
-// import { styled } from 'styled-components'
+import { styled } from 'styled-components'
+
+export const HomeContainer = styled.main`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  flex: 1;
+
+  form {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 3.5rem;
+  }
+`
+
+export const FormContainer = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 0.5rem;
+  flex-wrap: wrap;
+
+  color: ${(props) => props.theme['gray-100']};
+  font-size: 1.125rem;
+  font-weight: bold;
+`
+
+export const CountDownContainer = styled.div`
+  display: flex;
+  gap: 1rem;
+
+  font-family: 'Roboto Mono', monospace;
+  font-size: 10rem;
+  line-height: 8rem;
+  color: ${(props) => props.theme['gray-100']};
+
+  span {
+    background: ${(props) => props.theme['gray-700']};
+    padding: 2rem 1rem;
+    border-radius: 8px;
+  }
+`
+
+export const Separetor = styled.div`
+  display: flex;
+  justify-content: center;
+
+  width: 4rem;
+  padding: 2rem 0;
+
+  color: ${(props) => props.theme['green-500']};
+
+  overflow: hidden;
+`
+
+const BaseInput = styled.input`
+  height: 2.5rem;
+  padding: 0 0.5rem;
+
+  background: transparent;
+  color: ${(props) => props.theme['gray-100']};
+
+  border: 0;
+  border-bottom: 2px solid ${(props) => props.theme['gray-500']};
+  font-weight: bold;
+  font-size: 1.125rem;
+
+  &:focus {
+    box-shadow: none;
+    border-color: ${(props) => props.theme['green-500']};
+  }
+
+  &::placeholder {
+    color: ${(props) => props.theme['gray-500']};
+  }
+`
+
+export const TaskInput = styled(BaseInput)`
+  flex: 1;
+
+  &::-webkit-calendar-picker-indicator {
+    display: none !important;
+  }
+`
+
+export const MinutesAmountInput = styled(BaseInput)`
+  width: 4rem;
+`
+
+export const StarCountDownButton = styled.button`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 0.5rem;
+
+  width: 100%;
+  padding: 1rem;
+
+  border: none;
+  border-radius: 8px;
+
+  cursor: pointer;
+  background-color: ${(props) => props.theme['green-500']};
+  color: ${(props) => props.theme['gray-100']};
+
+  &:disabled {
+    opacity: 0.7;
+    cursor: not-allowed;
+  }
+
+  &:not(:disabled):hover {
+    background-color: ${(props) => props.theme['green-700']};
+  }
+`
