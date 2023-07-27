9725cf3b5fbc96c5c5ea80545c998fd6f3bdad4f
felipekgouvea <felipekinupesg@hotmail.com>
2023-07-27 13:05:17 -0300

    refactor: Header Component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,20 @@
+import { HeaderContainer } from './styles'
+import logoIgnite from '../../assets/logo-ignite.svg'
+import { Timer, Scroll } from 'phosphor-react'
+import { NavLink } from 'react-router-dom'
+
 export const Header = () => {
   return (
-    <div>
-      <h1>Header</h1>
-    </div>
+    <HeaderContainer>
+      <img src={logoIgnite} alt="" />
+      <nav>
+        <NavLink to="/" title="Timer">
+          <Timer size={24} />
+        </NavLink>
+        <NavLink to="/history" title="Histórico">
+          <Scroll size={24} />
+        </NavLink>
+      </nav>
+    </HeaderContainer>
   )
 }
